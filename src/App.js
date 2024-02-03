f473976104430482a53d05ba2a747f1978e2715d
jaquintero.celerix <jaquintero@celerix.com>
2024-02-03 15:31:37 -0500

    activity done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "./miCss.css"
+import "./miCss.css";
+import imageSisebuto from "./images/reyes/rey_sisebuto.png";
+import imageLeogivildo from "./images/reyes/rey_leogivildo.png";
+import imageAtanagildo from "./images/reyes/rey_atanagildo.png";
 
 function App() {
+  const kings = [
+    { name: "Sisebuto", image: imageSisebuto },
+    { name: "Leogivildo", image: imageLeogivildo },
+    { name: "Atanagildo", image: imageAtanagildo },
+  ];
   return (
-    <div className="App">
-      <h1 className="fondo">Curso de react</h1>
+    <div className="container">
+      {kings.map((king) => {
+        return (
+          <div className="card">
+            <img src={king.image} alt="" />
+            <div>{king.name}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
