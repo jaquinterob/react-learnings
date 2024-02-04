9c0d76b7fef4d5ef66307cc3df911f2a58d41ec9
jaquintero.celerix <jaquintero@celerix.com>
2024-02-03 23:22:04 -0500

    done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./miCss.css";
 import imageSisebuto from "./images/reyes/rey_sisebuto.png";
 import imageLeogivildo from "./images/reyes/rey_leogivildo.png";
 import imageAtanagildo from "./images/reyes/rey_atanagildo.png";
+import imageIncognito from "./images/reyes/rey_incognito.png";
 
 function App() {
   const kings = [
@@ -10,13 +11,31 @@ function App() {
     { name: "Leogivildo", image: imageLeogivildo },
     { name: "Atanagildo", image: imageAtanagildo },
   ];
+
+  const clickOnImg = (e) => {
+    if (e.target.src.includes("incognito")) {
+      e.target.style.visibility = "hidden"
+    } else {
+      e.target.src = imageIncognito;
+      e.target.parentNode.style.backgroundColor = "transparent";
+    }
+  };
+
+  const clickOnKingsName = (e) => {
+    if (e.target.innerHTML === "visto") {
+      e.target.innerHTML = "";
+    } else {
+      e.target.innerHTML = "visto";
+    }
+  };
+
   return (
     <div className="container">
       {kings.map((king) => {
         return (
-          <div className="card">
-            <img src={king.image} alt="" />
-            <div>{king.name}</div>
+          <div key={king.name} className="card">
+            <img src={king.image} alt="" onClick={clickOnImg} />
+            <div onClick={clickOnKingsName}>{king.name}</div>
           </div>
         );
       })}
