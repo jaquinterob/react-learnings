28426cd3b151dc33a5cd27ab06efc9278eeb717b
jaquintero.celerix <jaquintero@celerix.com>
2024-02-05 00:05:16 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-import "./miCss.css"
 
-function App() {
+const App = () => {
+  const reyesGodos = [
+    {
+      nombre: "Ataúlfo",
+      aficion: "comer toros sin pelar",
+    },
+    {
+      nombre: "Recesvinto",
+      aficion: "leer a Hegel en arameo",
+    },
+    {
+      nombre: "Teodorico",
+      aficion: "la cría del escarabajo en almíbar",
+    },
+  ];
+
+  let [message, setMessage] = useState("");
+  let [counter, setCounter] = useState(0);
+
+  const nextKing = () => {
+    setCounter(counter + 1);
+    if (counter + 1 >= reyesGodos.length) {
+      setCounter(0);
+    }
+    setMessage(
+      <div className="text">
+        La aficion principal de{" "}
+        <span className="rojo">{reyesGodos[counter].nombre}</span> es{" "}
+        <span className="verde">{reyesGodos[counter].aficion}</span>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <h1 className="fondo">Curso de react</h1>
-    </div>
+    <>
+      <button onClick={nextKing}>Ver siguiente</button>
+      <h1>{counter}</h1>
+      <h2>{message}</h2>
+    </>
   );
-}
-
+};
 export default App;
