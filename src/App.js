27c5a1f12e5854d86915f01ffc9681cef69e9731
jaquintero.celerix <jaquintero@celerix.com>
2024-02-06 14:28:21 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
-import "./miCss.css"
+import "./miCss.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="fondo">Curso de react</h1>
-    </div>
-  );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+      .then((response) => response.json())
+      .then((countries) => {
+        countries
+          .filter((country) => country.name.common.split(" ").length === 1)
+          .map((country, i) => {
+            setCountries((e) => [...e, makeACountry(country, i)]);
+          });
+      });
+  }, []);
+
+  const makeACountry = (country, i) => {
+    return (
+      <div key={i} className="card">
+        <div className="name">{country.name.common}</div>
+        <div>
+          <img className="flag" src={country.flags.svg} alt="" />
+        </div>
+      </div>
+    );
+  };
+
+  return <div className="main-container">{countries}</div>;
 }
 
 export default App;
